4881f9c18109bfc3e10982abd7e6cab8cc9b371f
Aviral Kaushik <zaviralkaushik@gmail.com>
2023-06-28 17:59:02 +0530

    Successfully adapted UI with API

---
+++ app/src/main/java/com/aviral/quickreport/Earthquake.java
@@ -0,0 +1,36 @@
+package com.aviral.quickreport;
+
+public class Earthquake {
+
+    private final double mMagnitude;
+
+    private final String mLocation;
+
+    private final long mTimeInMilliseconds;
+
+    private final String mUrl;
+
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    public double getmMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+}
